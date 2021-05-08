1c34b18d8fb5ef34974532555aaf6f053444f2cb
aka <aka@aka.com>
2021-05-08 13:26:11 +0300

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express")
-const dotenv = require("dotenv")
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 
 //Load config
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
 
 const app = express()
 
+const PORT = process.env.PORT
 
-
-app.listen()
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
