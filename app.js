cf6011acaf7c81c75692fab6e2c628b61ad73d56
Alex Alex <alex@MacBook-Pro-Alex-2.local>
2021-05-08 14:24:09 +0300

    create folder views/layouts

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 //Handlebars
-app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT
