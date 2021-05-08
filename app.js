8cae290be3c043945dbda00f4f773503b589b86c
Alex Alex <alex@MacBook-Pro-Alex-2.local>
2021-05-08 14:18:25 +0300

    add express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 
@@ -11,9 +12,14 @@ connectDB()
 
 const app = express()
 
+//Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+//Handlebars
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT
 
